e6eeb09157382d3d80084469800384a63fbbe2c8
Zhuyi Xue (Desktop) <zhuyi.xue@mail.utoronto.ca>
2013-11-18 15:51:50 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='mdtime',
+      version='1.0',
+      author='Zhuyi Xue',
+      email='zhuyi.xue@mail.utoronto.ca',
+      packages=find_packages(),
+      keywords = 'molecular dynamics gromacs time',
+      )
