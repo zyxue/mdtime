48ee05b05346fb8b02ef4fbdb6f3653e28d5e787
Zhuyi Xue (Desktop) <zhuyi.xue@mail.utoronto.ca>
2013-11-19 13:47:04 -0500

    updated setup.py to 1.0.1a1

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(name='mdtime',
-      version='1.0',
+      version='1.0.1a1',
       author='Zhuyi Xue',
-      email='zhuyi.xue@mail.utoronto.ca',
-      packages=find_packages(),
+      author_email='zhuyi.xue@mail.utoronto.ca',
+      url='http://www.zhuyixue.com',
       keywords = 'molecular dynamics gromacs time',
+      license='GPLv3',
+      description='mdtime checks the time of a tpr file or cpt file from Gromacs, or compare the two to see if time in cpt is less than that in tpr',
+      long_description='supposed to be a long description',
+      platforms=['unix-like'],
+
+      packages=find_packages(),
+      scripts=['mdtime.py']
       )
